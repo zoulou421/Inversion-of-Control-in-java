914ddebf066ebf38735a0d1c097e4689f1526799
Bonevy BEBY <bonevybeby@gmail.com>
2024-07-18 04:47:54 +0200

    ID by static instanciation

--- src/presentation/Pres1.java
+++ src/presentation/Pres1.java
@@ -6,6 +6,9 @@ import metier.MetierImpl;
 public class Pres1 {
 
 	public static void main(String[] args) {
+		/*
+		 * Dependency Injection by Static Instanciation 
+		 */
 		MetierImpl metier=new MetierImpl();
 		DaoImpl dao=new DaoImpl();
 		metier.setDao(dao);
